b2144b20d7fa989eec3af2c2e7d1d9fe82c602e6
Dean DeHart <DeHartDean@gmail.com>
2024-02-08 23:41:37 -0500

    Let's try adding deleteWord to handle ctrl + backspace

--- app/assets/javascripts/models/combobox/filtering.js
+++ app/assets/javascripts/models/combobox/filtering.js
@@ -1,57 +1,66 @@
+import Combobox from "models/combobox/base";
+import { applyFilter, nextFrame, debounce } from "helpers";
+import { get } from "vendor/requestjs";
 
-import Combobox from "models/combobox/base"
-import { applyFilter, nextFrame, debounce } from "helpers"
-import { get } from "vendor/requestjs"
-
-Combobox.Filtering = Base => class extends Base {
-  filter(event) {
-    if (this._isAsync) {
-      this._debouncedFilterAsync(event)
-    } else {
-      this._filterSync(event)
+Combobox.Filtering = (Base) =>
+  class extends Base {
+    filter(event) {
+      if (this._isAsync) {
+        this._debouncedFilterAsync(event);
+      } else {
+        this._filterSync(event);
+      }
     }
-  }
 
-  _initializeFiltering() {
-    this._debouncedFilterAsync = debounce(this._debouncedFilterAsync.bind(this))
-  }
-
-  _debouncedFilterAsync(event) {
-    this._filterAsync(event)
-  }
-
-  async _filterAsync(event) {
-    const q = this._actingCombobox.value.trim()
-
-    await get(this.asyncSrcValue, { responseKind: "turbo-stream", query: { q } })
-
-    this._afterTurboStreamRender(() => this._commitFilter(q, event))
-  }
-
-  _filterSync(event) {
-    const query = this._actingCombobox.value.trim()
-
-    this.open()
-
-    this._allOptionElements.forEach(applyFilter(query, { matching: this.filterableAttributeValue }))
-
-    this._commitFilter(query, event)
-  }
-
-  _commitFilter(query, event) {
-    const isDeleting = event.inputType === "deleteContentBackward"
-
-    if (this._isValidNewOption(query, { ignoreAutocomplete: isDeleting })) {
-      this._selectNew(query)
-    } else if (isDeleting) {
-      this._deselect()
-    } else {
-      this._select(this._visibleOptionElements[0])
+    _initializeFiltering() {
+      this._debouncedFilterAsync = debounce(
+        this._debouncedFilterAsync.bind(this)
+      );
     }
-  }
 
-  async _afterTurboStreamRender(callback) {
-    await nextFrame()
-    callback()
-  }
-}
+    _debouncedFilterAsync(event) {
+      this._filterAsync(event);
+    }
+
+    async _filterAsync(event) {
+      const q = this._actingCombobox.value.trim();
+
+      await get(this.asyncSrcValue, {
+        responseKind: "turbo-stream",
+        query: { q },
+      });
+
+      this._afterTurboStreamRender(() => this._commitFilter(q, event));
+    }
+
+    _filterSync(event) {
+      const query = this._actingCombobox.value.trim();
+
+      this.open();
+
+      this._allOptionElements.forEach(
+        applyFilter(query, { matching: this.filterableAttributeValue })
+      );
+
+      this._commitFilter(query, event);
+    }
+
+    _commitFilter(query, event) {
+      const isDeleting =
+        event.inputType === "deleteContentBackward" ||
+        event.inputType === "deleteWordBackward";
+
+      if (this._isValidNewOption(query, { ignoreAutocomplete: isDeleting })) {
+        this._selectNew(query);
+      } else if (isDeleting) {
+        this._deselect();
+      } else {
+        this._select(this._visibleOptionElements[0]);
+      }
+    }
+
+    async _afterTurboStreamRender(callback) {
+      await nextFrame();
+      callback();
+    }
+  };
--- app/assets/javascripts/models/combobox/navigation.js
+++ app/assets/javascripts/models/combobox/navigation.js
@@ -1,39 +1,40 @@
-import Combobox from "models/combobox/base"
-import { cancel } from "helpers"
+import Combobox from "models/combobox/base";
+import { cancel } from "helpers";
 
-Combobox.Navigation = Base => class extends Base {
-  navigate(event) {
-    if (this._autocompletesList) {
-      this._keyHandlers[event.key]?.call(this, event)
+Combobox.Navigation = (Base) =>
+  class extends Base {
+    navigate(event) {
+      if (this._autocompletesList) {
+        this._keyHandlers[event.key]?.call(this, event);
+      }
     }
-  }
 
-  _keyHandlers = {
-    ArrowUp: (event) => {
-      this._selectIndex(this._selectedOptionIndex - 1)
-      cancel(event)
-    },
-    ArrowDown: (event) => {
-      this._selectIndex(this._selectedOptionIndex + 1)
-      cancel(event)
-    },
-    Home: (event) => {
-      this._selectIndex(0)
-      cancel(event)
-    },
-    End: (event) => {
-      this._selectIndex(this._visibleOptionElements.length - 1)
-      cancel(event)
-    },
-    Enter: (event) => {
-      this.close()
-      this._actingCombobox.blur()
-      cancel(event)
-    },
-    Escape: (event) => {
-      this.close()
-      this._actingCombobox.blur()
-      cancel(event)
-    }
-  }
-}
+    _keyHandlers = {
+      ArrowUp: (event) => {
+        this._selectIndex(this._selectedOptionIndex - 1);
+        cancel(event);
+      },
+      ArrowDown: (event) => {
+        this._selectIndex(this._selectedOptionIndex + 1);
+        cancel(event);
+      },
+      Home: (event) => {
+        this._selectIndex(0);
+        cancel(event);
+      },
+      End: (event) => {
+        this._selectIndex(this._visibleOptionElements.length - 1);
+        cancel(event);
+      },
+      Enter: (event) => {
+        this.close();
+        this._actingCombobox.blur();
+        cancel(event);
+      },
+      Escape: (event) => {
+        this.close();
+        this._actingCombobox.blur();
+        cancel(event);
+      },
+    };
+  };
