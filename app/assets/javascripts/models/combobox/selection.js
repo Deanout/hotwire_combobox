b357e11bccee334677f5a7d2e0e2db8bc76ac341
Dean DeHart <DeHartDean@gmail.com>
2024-02-09 17:35:28 -0500

    Video commit #1

--- app/assets/javascripts/models/combobox/selection.js
+++ app/assets/javascripts/models/combobox/selection.js
@@ -1,76 +1,83 @@
-import Combobox from "models/combobox/base"
-import { wrapAroundAccess } from "helpers"
+import Combobox from "models/combobox/base";
+import { wrapAroundAccess } from "helpers";
 
-Combobox.Selection = Base => class extends Base {
-  selectOption(event) {
-    this._select(event.currentTarget)
-    this.close()
-  }
+Combobox.Selection = (Base) =>
+  class extends Base {
+    selectOption(event) {
+      // Direct user action via click
+      this._select(event.currentTarget);
 
-  _connectSelection() {
-    if (this.hasPrefilledDisplayValue) {
-      this._actingCombobox.value = this.prefilledDisplayValue
-    }
-  }
+      // Directly fire the event here, indicating a user-initiated action
+      this._fireChangeEvent(event.currentTarget);
 
-  _select(option, { force = false } = {}) {
-    this._resetOptions()
-
-    if (option) {
-      this._markValid()
-      this._maybeAutocompleteWith(option, { force })
-      this._commitSelection(option, { selected: true })
-    } else {
-      this._markInvalid()
-    }
-  }
-
-  _commitSelection(option, { selected }) {
-    this._markSelected(option, { selected })
-
-    if (selected) {
-      this.hiddenFieldTarget.value = option.dataset.value
-      option.scrollIntoView({ block: "nearest" })
-    } else {
-      this.hiddenFieldTarget.value = null
-    }
-  }
-
-  _markSelected(option, { selected }) {
-    if (this.hasSelectedClass) {
-      option.classList.toggle(this.selectedClass, selected)
+      // Close the listbox after a selection
+      this.close();
     }
 
-    option.setAttribute("aria-selected", selected)
-  }
-
-  _deselect() {
-    const option = this._selectedOptionElement
-    if (option) this._commitSelection(option, { selected: false })
-  }
-
-  _selectNew(query) {
-    this._resetOptions()
-    this.hiddenFieldTarget.value = query
-    this.hiddenFieldTarget.name = this.nameWhenNewValue
-  }
-
-  _selectIndex(index) {
-    const option = wrapAroundAccess(this._visibleOptionElements, index)
-    this._select(option, { force: true })
-  }
-
-  _preselectOption() {
-    if (this._hasValueButNoSelection && this._allOptions.length < 100) {
-      const option = this._allOptions.find(option => {
-        return option.dataset.value === this.hiddenFieldTarget.value
-      })
-
-      if (option) this._markSelected(option, { selected: true })
+    _connectSelection() {
+      if (this.hasPrefilledDisplayValue) {
+        this._actingCombobox.value = this.prefilledDisplayValue;
+      }
     }
-  }
 
-  get _hasValueButNoSelection() {
-    return this.hiddenFieldTarget.value && !this._selectedOptionElement
-  }
-}
+    _select(option, { force = false } = {}) {
+      this._resetOptions();
+
+      if (option) {
+        this._markValid();
+        this._maybeAutocompleteWith(option, { force });
+        this._commitSelection(option, { selected: true });
+      } else {
+        this._markInvalid();
+      }
+    }
+
+    _commitSelection(option, { selected }) {
+      this._markSelected(option, { selected });
+
+      if (selected) {
+        this.hiddenFieldTarget.value = option.dataset.value;
+        option.scrollIntoView({ block: "nearest" });
+      } else {
+        this.hiddenFieldTarget.value = null;
+      }
+    }
+
+    _markSelected(option, { selected }) {
+      if (this.hasSelectedClass) {
+        option.classList.toggle(this.selectedClass, selected);
+      }
+
+      option.setAttribute("aria-selected", selected);
+    }
+
+    _deselect() {
+      const option = this._selectedOptionElement;
+      if (option) this._commitSelection(option, { selected: false });
+    }
+
+    _selectNew(query) {
+      this._resetOptions();
+      this.hiddenFieldTarget.value = query;
+      this.hiddenFieldTarget.name = this.nameWhenNewValue;
+    }
+
+    _selectIndex(index) {
+      const option = wrapAroundAccess(this._visibleOptionElements, index);
+      this._select(option, { force: true });
+    }
+
+    _preselectOption() {
+      if (this._hasValueButNoSelection && this._allOptions.length < 100) {
+        const option = this._allOptions.find((option) => {
+          return option.dataset.value === this.hiddenFieldTarget.value;
+        });
+
+        if (option) this._markSelected(option, { selected: true });
+      }
+    }
+
+    get _hasValueButNoSelection() {
+      return this.hiddenFieldTarget.value && !this._selectedOptionElement;
+    }
+  };
